abdba0bcc9fd0cfeb26f8f2d71028cfd09da1afb
Trangmt <manhtrangtn@gmail.com>
2019-05-13 23:29:21 +0700

    start

---
+++ app.js
@@ -0,0 +1,46 @@
+var http  = require('http');
+var ex = require('express');
+var app = ex();
+app.use(ex.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+var users = [];
+var socketUsername;
+var server = http.Server(app).listen(3141);
+var io = require('socket.io')(server);
+io.on("connect", function (socket) {
+    console.log(socket.id + ' connected!');
+    socket.on('start', function (data) { 
+        if(users.indexOf(data)>=0){
+            socket.emit('false');
+        } else{
+            socketUsername = data;
+            users.push(data);
+            socket.emit('true',data);
+            io.sockets.emit('users', users);
+        }
+     })
+     socket.on('send',function (data) { 
+        io.sockets.emit('sent', data);
+      })
+    socket.on('logout', function (data) { 
+        users.splice(users.indexOf(data),1);
+        io.sockets.emit('users', users);
+     })
+     socket.on('stick',function (data) {
+         if (data=='agry') {
+             io.sockets.emit('stk','sticker/agry.png')
+         } else if (data == 'gift') {
+             io.sockets.emit('stk', 'sticker/gift.jpg')
+         } else if (data == 'leuuu') {
+             io.sockets.emit('stk', 'sticker/leuuuu.png')
+         } else if (data == 'love') {
+             io.sockets.emit('stk', 'sticker/love.png')
+         } else if (data == 'simon') {
+             io.sockets.emit('stk', 'sticker/simon.png')
+         } 
+     })
+})
+app.get('/',function (rq,rs) { rs.render('index') });
+
+console.log("server starting !")
---
+++ public/js/function.js
@@ -0,0 +1,74 @@
+$(function () {
+    $("#chat-box").hide();
+    $("#login-box").show();
+    $(".stickers").hide();
+    var socket = io.connect('http://localhost:3141'); 
+    var username;
+    socket.on('users', function (data) {
+        $("#onlineUser").html("");
+        data.forEach(function (element) {
+            $("#onlineUser").append('<div class="user">' + element + '</div>')
+        });
+    });
+    $("#start").click(function () { 
+        if($("#username").val()==''){alert("Please enter vaild username!")}else
+        socket.emit('start', $("#username").val());
+    });
+    socket.on('false', function () { 
+        alert("Username already exist! Pleas choose an others username.")
+    })
+    socket.on('true',function (data) { 
+        $("#login-box").hide(200);
+        $("#chat-box").show(400);
+        $("#cUser").text(data);
+        username = data;
+    })
+    $("#send").click(function () { 
+        if ($("#message").val()!='') {
+            socket.emit('send', {
+            username: username,
+            message: $("#message").val()
+        });
+        $("#message").val("")}else{}
+        
+    });
+    socket.on('sent',function (data) { 
+        $("#chat").append(
+            '<div class = "msg"><span class = "msgUser">' + data.username + ':</span><br><span class = "msgContent">' +data.message+'</span></div>'
+        );
+        $("#chat").animate({
+            scrollTop: $("#chat").height()
+        }, 100);
+    });
+    $("#logout").click(function () {
+            $("#chat-box").hide(200);
+            $("#login-box").show(400);
+            socket.emit('logout', username);
+    });
+    $(".stk").click(function () { 
+        $(".stickers").show(300);
+    })
+    $("#agry").click(function () { 
+         socket.emit('stick', "agry");
+    })
+    $("#gift").click(function () {
+        socket.emit('stick', "gift");
+    })
+    $("#leuuu").click(function () {
+        socket.emit('stick', "leuuu");
+    })
+    $("#love").click(function () {
+        socket.emit('stick', "love");
+    })
+    $("#simon").click(function () {
+        socket.emit('stick', "simon");
+    })
+    socket.on('stk', function (data) {
+        $("#chat").append(
+            '<img src="'+data+'" class="sent-img"/><br>'
+        );
+        $("#chat").animate({
+            scrollTop: $("#chat").height()
+        }, 100);
+    });
+});
